8cab8c9d5b614a9905fd19f3d3bd77330dbadd93
Nguyen Thai Son <sonnguyenthai@ncc.asia>
2023-05-26 20:07:35 +0700

    update

---
+++ resources/views/admin/dashboard/account.blade.php
@@ -0,0 +1,207 @@
+@extends('admin.dashboard.layout')
+@section('content')
+    <link href="{{ url('profile/demo/demo.css')  }}" rel="stylesheet"/>
+    <link href="{{ url('profile/css/paper-dashboard.css?v=2.0.1')  }}" rel="stylesheet"/>
+    <link href="{{ url('profile/css/bootstrap.min.css')  }}" rel="stylesheet"/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <div class="content">
+        <div class="row">
+            <div class="col-lg-3 col-md-6 col-sm-6">
+                <div class="card card-stats">
+                    <div class="card-body ">
+                        <div class="row">
+                            <div class="col-5 col-md-4">
+                                <div class="icon-big text-center icon-warning">
+                                    <i class="nc-icon nc-globe text-warning"></i>
+                                </div>
+                            </div>
+                            <div class="col-7 col-md-8">
+                                <div class="numbers">
+                                    <p class="card-category">Tổng số người dùng</p>
+                                    <p class="card-title" id="all_post">
+                                    <p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card-footer ">
+                        <hr>
+                        <div class="stats">
+                            <i class="fa fa-refresh"></i>
+                            Hiện tại
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 col-sm-6">
+                <div class="card card-stats">
+                    <div class="card-body ">
+                        <div class="row">
+                            <div class="col-5 col-md-4">
+                                <div class="icon-big text-center icon-warning">
+                                    <i class="nc-icon nc-money-coins text-success"></i>
+                                </div>
+                            </div>
+                            <div class="col-7 col-md-8">
+                                <div class="numbers">
+                                    <p class="card-category">Đã phê duyệt</p>
+                                    <p class="card-title" id="count_post_approved">
+                                    <p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card-footer ">
+                        <hr>
+                        <div class="stats">
+                            <i class="fa fa-calendar-o"></i>
+                            Hiện tại
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 col-sm-6">
+                <div class="card card-stats">
+                    <div class="card-body ">
+                        <div class="row">
+                            <div class="col-5 col-md-4">
+                                <div class="icon-big text-center icon-warning">
+                                    <i class="nc-icon nc-vector text-danger"></i>
+                                </div>
+                            </div>
+                            <div class="col-7 col-md-8">
+                                <div class="numbers">
+                                    <p class="card-category">Chưa phê duyệt</p>
+                                    <p class="card-title" id="count_post_not_approved">
+                                    <p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card-footer ">
+                        <hr>
+                        <div class="stats">
+                            <i class="fa fa-clock-o"></i>
+                            Hiện tại
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 col-sm-6">
+                <div class="card card-stats">
+                    <div class="card-body ">
+                        <div class="row">
+                            <div class="col-5 col-md-4">
+                                <div class="icon-big text-center icon-warning">
+                                    <i class="nc-icon nc-favourite-28 text-primary"></i>
+                                </div>
+                            </div>
+                            <div class="col-7 col-md-8">
+                                <div class="numbers">
+                                    <p class="card-category">Bài viết đã xoá</p>
+                                    <p class="card-title" id="count_post_trashed">
+                                    <p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card-footer ">
+                        <hr>
+                        <div class="stats">
+                            <i class="fa fa-refresh"></i>
+                            Hiện tại
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="content" style="margin-top: 5px">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">
+                        <h4 class="card-title"> Danh sách bài viết chưa phê duyệt</h4>
+                    </div>
+                    <div class="card-body">
+                        <div class="table-responsive">
+                            <table class="table">
+                                <thead class=" text-primary">
+                                <th>Tên công ty</th>
+                                <th>Ngày đăng</th>
+                                <th class="text-center">Chức năng</th>
+                                </thead>
+                                <tbody id="post-not-approved">
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="content" style="margin-top: 5px">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">
+                        <h4 class="card-title"> Danh sách bài viết đã phê duyệt </h4>
+                    </div>
+                    <div class="card-body">
+                        <div class="table-responsive">
+                            <table class="table">
+                                <thead class=" text-primary">
+                                <th>Tên công ty</th>
+                                <th>Ngày đăng</th>
+                                <th>Phê duyệt</th>
+                                <th class="text-center">Chức năng</th>
+                                </thead>
+                                <tbody id="post-approved">
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="content" style="margin-top: 5px">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">
+                        <h4 class="card-title"> Danh sách bài viết đã xoá </h4>
+                    </div>
+                    <div class="card-body">
+                        <div class="table-responsive">
+                            <table class="table">
+                                <thead class=" text-primary">
+                                <th>Tên công ty</th>
+                                <th>Ngày xoá</th>
+                                <th>Phê duyệt</th>
+                                <th>Người xoá</th>
+                                <th class="text-center">Chức năng</th>
+                                </thead>
+                                <tbody id="post-trashed">
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
+<script src="{{ url('profile/js/core/jquery.min.js') }}"></script>
+<script src="{{ url('profile/js/core/popper.min.js') }}"></script>
+<script src="{{ url('profile/js/core/bootstrap.min.js') }}"></script>
+<script src="{{ url('profile/js/plugins/perfect-scrollbar.jquery.min.js') }}"></script>
+<!-- Chart JS -->
+<script src="{{ url('profile/js/plugins/chartjs.min.js') }}"></script>
+<!--  Notifications Plugin    -->
+<script src="{{ url('profile/js/plugins/bootstrap-notify.js') }}"></script>
+<!-- Control Center for Now Ui Dashboard: parallax effects, scripts for the example pages etc -->
+<script src="{{ url('profile/js/paper-dashboard.min.js?v=2.0.1') }}" type="text/javascript"></script>
+<!-- Paper Dashboard DEMO methods, don't include it in your project! -->
+<script src="{{ url('profile/demo/demo.js')  }}"></script>
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
