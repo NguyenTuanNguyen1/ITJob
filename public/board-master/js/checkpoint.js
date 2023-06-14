211d85b072b6f8aaa1e8d1ffe1eb8c2c8abcf95d
ConCongSan <tranlequangthinh24122002@gmail.com>
2023-06-14 19:02:27 +0700

    update

--- public/board-master/js/checkpoint.js
+++ public/board-master/js/checkpoint.js
@@ -1,37 +1,17 @@
-feather.replace();    
+$(document).ready(function () {
+    $('.toggle input[type="checkbox"]').click(function () {
 
-    $(document).ready(function(){
-        $('.toggle input[type="checkbox"]').click(function(){
-            $(this).parent().toggleClass('on');
-
-            if ($(this).parent().hasClass('on')) {
-                $(this).parent().children('.label').text('Hiện bài viết')
-            } else {
-                $(this).parent().children('.label').text('Ẩn bài viết')
-            }
-        });
-
-        // $('.checkbox input[type="checkbox"]').click(function(){
-        //     $(this).parent().toggleClass('on');
-
-        //     if ($(this).parent().hasClass('on')) {
-        //         $(this).parent().children('.label').text('On')
-        //     } else {
-        //         $(this).parent().children('.label').text('Off')
-        //     }
-        // });
-
-        // $('.radio input[type="radio"]').click(function(){
-        //     $(this).parent().addClass('on');
-
-        //     if ($(this).parent().hasClass('on')) {
-        //         $(this).parent().children('.label').text('On')
-        //     }
-        // });
-        $('input').focusin (function() {
-            $(this).parent().addClass('focus');
-        });
-        $('input').focusout (function() {
-            $(this).parent().removeClass('focus');
-        });
+        $(this).parent().toggleClass('on')
+        if ($(this).parent().hasClass('on')) {
+            $(this).parent().children('.label').text('Hiện bài viết')
+        } else {
+            $(this).parent().children('.label').text('Ẩn bài viết')
+        }
     });
+    $('input').focusin(function () {
+        $(this).parent().addClass('focus');
+    });
+    $('input').focusout(function () {
+        $(this).parent().removeClass('focus');
+    });
+});
